cffc3881b913eebda8f38d7b5877f17fba0c0b2f
Gus Humphrey <ghumphrey232@gmail.com>
2016-09-28 16:10:36 -0400

    Add variable exercise

---
+++ variable-playground.js
@@ -0,0 +1,24 @@
+// var person = {
+//     name: 'Gus',
+//     age: 17
+// };
+
+// function updatePerson(obj) {
+//     obj = {
+//         name: 'Gus',
+//         age: 20
+//     };
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [55, 98];
+
+function addGrades(gradesArr) {
+    gradesArr.push(82);
+    debugger;
+}
+
+addGrades(grades);
+console.log(grades);
