8902878c9c4184523544f634d43d4136c62c118a
Gus Humphrey <ghumphrey232@gmail.com>
2016-10-12 13:52:59 -0400

    Convert PUT /todos/:id to sequelize

--- server.js
+++ server.js
@@ -58,17 +58,6 @@ app.post('/todos', function(req, res) {
     }).catch (function(e) {
         return res.status(400).json(e);
     });
-
-    // if (!_.isBoolean(body.completed)  || !_.isString(body.description) || body.description.trim().length === 0) {
-    //     return res.status(400).send();
-    // }
-    // body.description = body.description.trim();
-
-    // body.id = todoNextId++;
-
-    // todos.push(body);
-
-    // res.json(body);
 });
 
 app.delete('/todos/:id', function(req,res) {
@@ -90,39 +79,34 @@ app.delete('/todos/:id', function(req,res) {
         res.status(500).send();
     });
 
-    // if (!matchedTodo) {
-    //     res.status(404).json({"error": "no todo found with that id"});
-    // } else {
-    //     todos = _.without(todos, matchedTodo);
-    //     res.json(todos);
-    // }
-
 });
 
 app.put('/todos/:id', function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
-    var validAttributes = {};
+    var attributes = {};
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
 
-    if(!matchedTodo) {
-        return res.status(404).send();
+    if (body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
     }
 
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validAttributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')) {
-        return res.status(400).send();
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
     }
 
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-        validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        return res.status(400).send();
-    }
-
-    _.extend(matchedTodo, validAttributes);
-    res.json(matchedTodo);
+    db.todo.findById(todoId).then(function (todo) {
+        if (todo) {
+            todo.update(attributes).then(function (todo) {
+                res.json(todo.toJSON());
+            }, function (e) {
+                res.status(400).json(e);
+            });
+        } else {
+            res.status(404).send();
+        }
+    }, function(e) {
+        res.status(500).send();
+    })
 });
 
 db.sequelize.sync().then(function() {
